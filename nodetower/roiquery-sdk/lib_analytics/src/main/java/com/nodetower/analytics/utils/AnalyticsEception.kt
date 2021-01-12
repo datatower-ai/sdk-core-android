cb3415132d3125fc1205962fe05426cafb3c8b46
zhouguoju <zhouguoju@nodetower.com>
2021-01-12 12:14:47 +0800

    rm

--- nodetower/roiquery-sdk/lib_analytics/src/main/java/com/nodetower/analytics/utils/AnalyticsEception.kt
+++
@@ -1,9 +0,0 @@
-package com.nodetower.analytics.utils
-
-/**
- * EventName, Properties Key/Value 格式错误
- */
-class InvalidDataException : Exception {
-    constructor(error: String?) : super(error)
-    constructor(throwable: Throwable?) : super(throwable)
-}
--- nodetower/roiquery-sdk/lib_base/src/main/java/com/nodetower/base/c.kt
+++
@@ -1,4 +0,0 @@
-package com.nodetower.base
-
-class c {
-}
