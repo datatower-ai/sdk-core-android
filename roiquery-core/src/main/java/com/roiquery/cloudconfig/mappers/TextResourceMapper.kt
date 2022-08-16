4e8db2e8e538e84da9cc17bddcab4146e9ae4e4a
xiaosailing <xiaosailing@nodetower.com>
2022-08-16 18:44:06 +0800

    数据检验向下版本兼容

--- roiquery-core/src/main/java/com/roiquery/cloudconfig/mappers/TextResourceMapper.kt
+++ roiquery-core/src/main/java/com/roiquery/cloudconfig/mappers/TextResourceMapper.kt
@@ -1,11 +1,13 @@
 package com.roiquery.cloudconfig.mappers
 
 import com.roiquery.cloudconfig.core.ResourceMapper
+import com.roiquery.cloudconfig.utils.AESCoder
 import java.io.ByteArrayInputStream
 import java.io.InputStream
 
 object TextResourceMapper : ResourceMapper {
-    override fun <T> toRepository(config: T, key: ByteArray): InputStream = ByteArrayInputStream(AESCoder.encrypt((config as String).toByteArray(),key))
+    override fun <T> toRepository(config: T, key: ByteArray): InputStream = ByteArrayInputStream(
+        AESCoder.encrypt((config as String).toByteArray(),key))
 //        (config as String).byteInputStream()
 
     @Suppress("Unchecked_Cast")
