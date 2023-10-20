94db2076f2e7a6b83796da4750236a9ffd4ba2cf
linkailong <linkailong@lovinjoy.com>
2023-10-20 19:01:04 +0800

    userAppend 和 userUniqAppend 加入参数检查

---
+++ roiquery-core/src/main/java/com/roiquery/analytics/utils/DataCheck.kt
@@ -0,0 +1,12 @@
+package com.roiquery.analytics.utils
+
+import org.json.JSONObject
+
+object DataCheck {
+    fun JSONObject.findFirstNonJsonArray(): String? {
+        keys().forEach {
+            optJSONArray(it) ?: return it
+        }
+        return null
+    }
+}
