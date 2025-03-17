86a2be64ce72738a62f1a9b097c0c154dd2a4a81
jokery.zhou <zhoujie@datatower.ai>
2025-03-17 16:13:02 +0800

    feat: Bump version to 3.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         set("minSdkVersion", 19)
         set("targetSdkVersion", 33)
 
-        set("dtsdkCoreVersionName", "3.1.0")
+        set("dtsdkCoreVersionName", "3.2.0")
 
         set("javaVersion", JavaVersion.VERSION_1_8)
         set("kotlinVersion", "1.8.21")
